d954c1802751dddb3f8c0ea0f44b3a8892bac205
Lautaro Bachmann <laurigbach@gmail.com>
2022-10-23 12:18:23 -0300

    Crear funcion para calcular promedio segun nombre de proceso

--- graph.py
+++ graph.py
@@ -11,6 +11,10 @@ def draw_basic_plot(xs, ys, label, color=None):
         plt.plot(xs, ys, label=label)
 
 
+def draw_bar_plot(groups, data):
+    plt.bar(groups, data, width=0.25)
+
+
 def change_plot_visuals():
     plt.legend()
     plt.grid()
@@ -35,26 +39,29 @@ def read_experiment_data_from_json(filename: str) -> list[dict]:
         return parsed_json
 
 
-def get_value_from_content(content):
-    return int(content.split(" ")[1])
+def get_average(output):
+    if output:
+        values = [int(value["content"].split(" ")[1]) for value in output]
+        return round(sum(values) / len(values), 2)
 
 
-def get_experiment_data(values):
-    xs = [value["time"] for value in values]
-    ys = [get_value_from_content(value["content"]) for value in values]
-    return xs, ys
+def get_averages(experiment_data, process_name):
+    result = []
+    for experiment in experiment_data:
+        averages = [
+            get_average(value)
+            for process, value in experiment["output"].items()
+            if process_name in process
+        ]
+        result.append(averages)
+    return result
 
 
 def main():
     data = read_experiment_data_from_json(JSON_FILE)
     os.makedirs("graphs", exist_ok=True)
-    for index, experiment in enumerate(data):
-        for process, values in experiment["output"].items():
-            if not values:
-                continue
-            xs, ys = get_experiment_data(values)
-            draw_basic_plot(xs, ys, process)
-        save_fig_to_path(f"graphs/graph{index}.png")
+    iobench = get_averages(data, "iobench")
+    cpubench = get_averages(data, "cpubench")
 
 
 if __name__ == "__main__":
